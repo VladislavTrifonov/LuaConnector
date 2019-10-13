e2b04025362331bd74bdb11cfdad7125fab42689
Vladislav Trifonov <vtrifonov548@gmail.com>
2019-10-13 20:53:28 +0300

    Processing files and Exceptions

--- LuaConnector/LuaState.cs
+++ LuaConnector/LuaState.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using LuaConnector.LuaTypes;
+using LuaConnector.Exceptions;
 
 namespace LuaConnector
 {
     public class LuaState : IDisposable
     {
         private bool disposed = false;
-        private readonly IntPtr lua_State; 
-        public LuaState()
-        {
+        private readonly IntPtr lua_State;
 
+        private List<string> files = new List<string>();
+
+        public LuaState(bool loadLibs = true)
+        {
+            lua_State = CApi.luaL_newstate();
+
+            if (loadLibs)
+                CApi.luaL_openlibs(lua_State);
         }
 
         ~LuaState()
@@ -34,8 +41,22 @@ namespace LuaConnector
 
             if (disposing)
             {
-                
+                // releasing managed resources
             }
+
+            // unmanaged resources
+            if (lua_State != IntPtr.Zero)
+                CApi.lua_close(lua_State);
+        }
+
+        public void ProcessFile(string path, string mode = null)
+        {
+            bool status = CApi.luaL_loadfilex(lua_State, path, null) == 1;
+
+            if (!status)
+                throw new ProcessFileException("Something wrong...");
+
+            files.Add(path);
         }
     }
 }
