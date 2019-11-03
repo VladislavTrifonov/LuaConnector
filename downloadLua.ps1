7e6dc65b319c11c9274386e902fe5c41f8543fb5
Evgeny Bogdanov <vtrifonov548@gmail.com>
2019-11-04 00:24:37 +0300

    Update azure pipelines config

--- downloadLua.ps1
+++ downloadLua.ps1
@@ -6,4 +6,4 @@ $client.DownloadFile("https://netix.dl.sourceforge.net/project/luabinaries/5.3.5
 [System.IO.Compression.ZipFile]::ExtractToDirectory("lua-5.3.5_Win32_bin.zip", "$PSScriptRoot")
 
 Write-Host "$Env:BUILD_BINARIESDIRECTORY"
-gci $Env:BUILD_BINARIESDIRECTORY
+gci $Env:BUILD_SOURCESDIRECTORY/LuaConnector
