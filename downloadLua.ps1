1e781d8c6a63dc9b0d98dddf1bfb1e4be07dd8a7
Evgeny Bogdanov <vtrifonov548@gmail.com>
2019-11-03 23:55:04 +0300

    Update azure pipelines config

--- downloadLua.ps1
+++ downloadLua.ps1
@@ -1,5 +1,5 @@
 $client = new-object System.Net.Webclient
-$client.DownloadFile("https://netix.dl.sourceforge.net/project/luabinaries/5.3.5/Tools%20Executables/lua-5.3.5_Win32_bin.zip", "$(System.DefaultWorkingDirectory)/lua-5.3.5_Win32_bin.zip")
+$client.DownloadFile("https://netix.dl.sourceforge.net/project/luabinaries/5.3.5/Tools%20Executables/lua-5.3.5_Win32_bin.zip", "lua-5.3.5_Win32_bin.zip")
 Write-Host "Hello World from $Env:AGENT_NAME."
 Write-Host "My ID is $Env:AGENT_ID."
 Write-Host "AGENT_WORKFOLDER contents:"
@@ -9,4 +9,3 @@ gci $Env:AGENT_BUILDDIRECTORY
 Write-Host "BUILD_SOURCESDIRECTORY contents:"
 gci $Env:BUILD_SOURCESDIRECTORY
 Write-Host "Over and out."
-Write-Host "defaultworkingdirectory is $(System.DefaultWorkingDirectory)"
