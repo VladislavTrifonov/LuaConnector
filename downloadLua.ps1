8b856c18b3cc73fb8bc5b727ede1de394cd976b6
Evgeny Bogdanov <vtrifonov548@gmail.com>
2019-11-04 00:12:09 +0300

    Update azure pipelines config

--- downloadLua.ps1
+++ downloadLua.ps1
@@ -1,11 +1,8 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
 $client = new-object System.Net.Webclient
 $client.DownloadFile("https://netix.dl.sourceforge.net/project/luabinaries/5.3.5/Tools%20Executables/lua-5.3.5_Win32_bin.zip", "lua-5.3.5_Win32_bin.zip")
-Write-Host "Hello World from $Env:AGENT_NAME."
-Write-Host "My ID is $Env:AGENT_ID."
-Write-Host "AGENT_WORKFOLDER contents:"
-gci $Env:AGENT_WORKFOLDER
-Write-Host "AGENT_BUILDDIRECTORY contents:"
-gci $Env:AGENT_BUILDDIRECTORY
-Write-Host "BUILD_SOURCESDIRECTORY contents:"
-gci $Env:BUILD_SOURCESDIRECTORY
-Write-Host "Over and out."
+
+[System.IO.Compression.ZipFile]::ExtractToDirectory("lua-5.3.5_Win32_bin.zip", "$PSScriptRoot")
+
+Write-Host "$(Agent.BuildDirectory)"
